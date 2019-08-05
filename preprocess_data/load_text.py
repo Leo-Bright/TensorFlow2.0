46701224e485619f3a45454ae36bbf3ef3cffd21
Leo <xinming.lai@qq.com>
2019-08-05 23:33:27 +0800

    feature: init load_text function

---
+++ preprocess_data/load_text.py
@@ -0,0 +1,16 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import os
+
+DIRECTORY_URL = 'https://storage.googleapis.com/download.tensorflow.org/data/illiad/'
+FILE_NAMES = ['cowper.txt', 'derby.txt', 'butler.txt']
+
+for name in FILE_NAMES:
+    text_dir = tf.keras.utils.get_file(name, origin=DIRECTORY_URL + name)
+
+parent_dir = os.path.dirname(text_dir)
+
+parent_dir
+
